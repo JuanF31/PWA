410c34a94675a3c0a5ed71c8db8836465f8aa712
JuanF31 <sebashernandez03@hotmail.com>
2023-08-31 10:44:30 -0600

    Promise

--- 01-fundamentos/prom-2.js
+++ 01-fundamentos/prom-2.js
@@ -1,5 +1,9 @@
 function sumarUno( numero ){
     var promesa = new Promise(function(resolve, reject){
+        console.log(numero);
+        if(numero >= 7){
+            reject('El número es muy alto');
+        }
         setTimeout(function() {
             resolve(numero + 1);
         }, 800)
@@ -8,14 +12,14 @@ function sumarUno( numero ){
 }
 
 
-sumarUno( 5 ).then( nuevoNumero => {
-    console.log(nuevoNumero);
-    return sumarUno(nuevoNumero);
-})
-.then( nuevoNumero => {
-    console.log(nuevoNumero)
-    return sumarUno(nuevoNumero);
-})
-.then( nuevoNumero => {
-    console.log(nuevoNumero);
-})
+sumarUno( 5 )
+    .then( sumarUno )
+    .then( sumarUno )
+    .then( sumarUno )
+    .then( nuevoNumero => {
+        console.log(nuevoNumero);
+    })
+    .catch(error => {
+        console.log('ERROR EN PROMESA')
+        console.log(error)
+    });
